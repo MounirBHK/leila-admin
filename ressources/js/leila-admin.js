d04d64d1f0179a4eb234351bcd6cbb2c05d02f13
Camille Semaan <cdem@xvitae.com>
2022-06-20 18:00:20 -0400

    Prototype de l'application cliente arrêtée : à compléter dans le TP1

--- ressources/js/leila-admin.js
+++ ressources/js/leila-admin.js
@@ -1,22 +1,35 @@
+let nomCollection = "";
+
+function rangeeEnJson(rangee) {
+    const lesElts = rangee.querySelectorAll('input');
+    let objetDonnees = {};
+    for(const elt of lesElts) {
+        objetDonnees[elt.name] = elt.value;
+    }
+    return JSON.stringify(objetDonnees);
+}
+
+
 document.querySelectorAll('.navigation-principale li').forEach(function(item, pos, itemsMenu) {
     item.addEventListener('click', function() {
         itemsMenu.forEach((elt)=>elt.classList.remove('actif'));
         item.classList.add('actif');
-        obtenirEnregistrements(item.dataset.collection);
+        nomCollection = item.dataset.collection;
+        obtenirEnregistrements();
     });
 });
 
 // Remarquez l'utilisation de async/await au lieu de then() pour gérer les promesses
-async function obtenirEnregistrements(nomCollection) {
+async function obtenirEnregistrements() {
     let reponse = await fetch('http://127.0.0.10:9000/index.php/' + nomCollection);
     // Traiter la réponse...
     let reponseJson = await reponse.json();
     // console.log('Nom de la collection : ', nomCollection);
     console.log('Collection (JSON) : ', reponseJson);
-    afficherCollection(nomCollection, reponseJson);
+    afficherCollection(reponseJson);
 }
 
-function afficherCollection(nomCollection, dataCollection) {
+function afficherCollection(dataCollection) {
     document.querySelector('.liste-enregistrements caption code').innerText = nomCollection;
     let rangeeTHead = document.querySelector('.liste-enregistrements thead tr');
     let tBody = document.querySelector('.liste-enregistrements tbody');
@@ -42,7 +55,13 @@ function afficherCollection(nomCollection, dataCollection) {
         let td = null;
         for(const prop in article) {
             td = document.createElement('td');
-            td.innerText = article[prop];
+            if(prop.match(/_id$/)) {
+                td.innerText = article[prop];
+            }
+            else {
+                td.innerHTML = `<input type="text" name="${prop}" value="${article[prop]}">`;
+            }
+            
             rangeeTBody.appendChild(td);
         }
         td = document.createElement('td');
@@ -54,20 +73,37 @@ function afficherCollection(nomCollection, dataCollection) {
 }
 
 document.querySelector('.liste-enregistrements').addEventListener('click', function(evt) {
+    const rangee = evt.target.closest('tr');
     if(evt.target && evt.target.classList.contains('btn-modifier')) {
-        modifierElement(evt.target.closest('tr').dataset.id);
+        modifierElement(rangee);
     }
     else if(evt.target && evt.target.classList.contains('btn-supprimer')) {
-        supprimerElement(evt.target.closest('tr').dataset.id);
+        supprimerElement(rangee);
     }
 });
 
-
-function modifierElement(idElt) {
-    console.log("Identifiant de l'élément à modifier : ", idElt);
+async function modifierElement(rangee) {
+    const donneesJson = rangeeEnJson(rangee);
+    //console.log("Données de la rangée en JSON", donneesJson);
+    let reponse = await fetch(
+        'http://127.0.0.10:9000/index.php/' + nomCollection + '/' + rangee.dataset.id,
+        {
+            method: 'PUT',
+            body: donneesJson
+        }
+    );
+    let reponseJson = await reponse.json();
 }
 
-function supprimerElement(idElt) {
-    console.log("Identifiant de l'élément à supprimer : ", idElt);
+async function supprimerElement(rangee) {
+    let reponse = await fetch(
+            'http://127.0.0.10:9000/index.php/' + nomCollection + '/' + rangee.dataset.id,
+            { method: 'DELETE' }
+    );
+    let reponseJson = await reponse.json();
+    // Supprimer la rangée localement (DOM)
+    if(reponseJson && reponseJson.nombre > 0) {
+        rangee.remove();
+    }
 }
 
